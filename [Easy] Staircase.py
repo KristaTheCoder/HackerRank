7ecc84808a407f8eff0bb622e261b5590973dceb
Krista Shuckerow <kshuckerow@gmail.com>
2016-09-27 00:18:36 -0400

    Proper spacing
    Gets rid of an extra line. 
    Removes extra white spacing and lines caused by not using commas after statements

--- [Easy] Staircase.py
+++ [Easy] Staircase.py
@@ -2,15 +2,16 @@
 
 import sys
 
-
 n = int(raw_input().strip())
 numSpace = n
 numHash = 1
-while(numSpace >= 0):
+while(numSpace > 0):
     for i in range(numSpace):
-        print "",
+        print '',
     for i in range(numHash):
-        print "#"
+        sys.stdout.write("#"), #Needs comma or else there will be a new line
+                               #Needs sys.stdout.write to remove any white space inbetween the characters
     numSpace = numSpace - 1
     numHash = numHash + 1
+    print ""
     
